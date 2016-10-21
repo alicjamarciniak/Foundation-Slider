5a4e4438317f31779bc05a4385bfcf2cf1195d1c
alicjamarciniak <lcm@3a.pl>
2016-10-21 14:31:11 +0200

    develop/fix: sticky navbar (in progress) and changed javascript file

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  /* navbar START */
+  function displayNavbarSticky() {
+
+      if (($(window).scrollTop()) > num || ($(window).innerWidth() < 640)) {
+          $('.navbar').hide();
+          $('.title-bar').show();
+          $('.title-bar').addClass('fixed');
+          $('.header').addClass('header-fixed');
+          $('.header__logo').addClass('header__logo-fixed');
+          $('.my-slider').addClass('slider-sticky');
+
+      } else if($(window).scrollTop() < num) {
+          $('.title-bar').hide();
+          $('.navbar').show();
+          $('.title-bar').removeClass('fixed');
+          $('.header').removeClass('header-fixed');
+          $('.header__logo').removeClass('header__logo-fixed');
+          $('.my-slider').removeClass('slider-sticky');
+      }
+  };
+  displayNavbarSticky();
+  var num = 10; //number of pixels before modifying styles
+  $(window).bind('scroll', displayNavbarSticky);
+  /* navbar END */
+
+  /* articles START */
+    $('.article-hexagon').on('click', function() {
+      $(this).siblings('.article-content').slideToggle('slow');
+    });
+  /* articles END */
+});
+/* FOUNDATION */
+  $(document).foundation();
+/* FOUNDATION */
--- js/slider.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-  /* articles START */
-    $('.article-hexagon').on('click', function() {
-      $(this).siblings('.article-content').slideToggle('slow');
-    });
-  /* articles END */
-});
