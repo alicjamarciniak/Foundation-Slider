21baf9ae8d3273b87c2b48f79d0c3733bfa08a9a
alicjamarciniak <lcm@3a.pl>
2016-10-24 17:04:16 +0200

    develop/fix: (in progress) fixing responsive & sticky navigation bar

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
   /* navbar START */
-  function displayNavbarSticky() {
-
-      if (($(window).scrollTop()) > num || ($(window).innerWidth() < 640)) {
-          // $('.example-menu').hide();
-          $('.title-bar').show();
-          $('.title-bar').addClass('fixed');
-          $('.header').addClass('header-fixed');
-          $('.header__logo').addClass('header__logo-fixed');
-          $('.my-slider').addClass('slider-sticky');
-
-      } else if($(window).scrollTop() < num) {
-          $('.title-bar').hide();
-          // $('.example-menu').show();
-          $('.title-bar').removeClass('fixed');
-          $('.header').removeClass('header-fixed');
-          $('.header__logo').removeClass('header__logo-fixed');
-          $('.my-slider').removeClass('slider-sticky');
-      }
-  };
-  displayNavbarSticky();
-  var num = 10; //number of pixels before modifying styles
-  $(window).bind('scroll', displayNavbarSticky);
+  // function displayNavbarSticky() {
+  //
+  //     if (($(window).scrollTop()) > num || ($(window).innerWidth() < 640)) {
+  //         // $('.example-menu').hide();
+  //         $('.title-bar').show();
+  //         $('.title-bar').addClass('fixed');
+  //         $('.header').addClass('header-fixed');
+  //         $('.header__logo').addClass('header__logo-fixed');
+  //         $('.my-slider').addClass('slider-sticky');
+  //
+  //     } else if($(window).scrollTop() < num) {
+  //         $('.title-bar').hide();
+  //         // $('.example-menu').show();
+  //         $('.title-bar').removeClass('fixed');
+  //         $('.header').removeClass('header-fixed');
+  //         $('.header__logo').removeClass('header__logo-fixed');
+  //         $('.my-slider').removeClass('slider-sticky');
+  //     }
+  // };
+  // displayNavbarSticky();
+  // var num = 10; //number of pixels before modifying styles
+  // $(window).bind('scroll', displayNavbarSticky);
   /* navbar END */
 
   /* articles START */
