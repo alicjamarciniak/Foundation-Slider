41bac7d54eda4addf7a57e4579bf2772b095a2a2
alicjamarciniak <marciniak.al@gmail.com>
2016-10-31 01:40:45 +0100

    develop/fix: Fixed code style

--- js/script.js
+++ js/script.js
@@ -1,47 +1,19 @@
 $(document).ready(function() {
   /* navbar START */
 
-  function displayToggleMenu() {
-    $('.title-bar').show();
-  }
-  function removeToggleMenu() {
-    $('.title-bar').hide();
-  }
-
-  function displayToggleNavbar() {
-    $('.navbar').addClass('navbar-fixed');
-  }
-  function removeToggleNavbar() {
-    $('.navbar').removeClass('navbar-fixed');
-  }
-
-  function displayStickyMenu() {
-    $('.header').addClass('header-fixed');
-    $('.header__logo').addClass('header__logo-fixed');
-    $('.my-slider').addClass('slider-sticky');
-    $('.menu-bar').addClass('fixed');
-  }
-  function removeStickyMenu() {
-    $('.header').removeClass('header-fixed');
-    $('.header__logo').removeClass('header__logo-fixed');
-    $('.my-slider').removeClass('slider-sticky');
-    $('.menu-bar').removeClass('fixed');
-  }
-
   function navbarHandler() {
 
       if ($(window).innerWidth() < 640) {
-          displayToggleMenu();
-          displayToggleNavbar();
-          displayStickyMenu();
+          $('.title-bar').show();
+          $('.navbar').addClass('navbar-fixed');
+          $(document.body).addClass('sticky-menu');
       } else if($(window).scrollTop() > num && ($(window).innerWidth() > 640)) {
-          removeToggleMenu();
-          removeToggleNavbar();
-          displayStickyMenu();
+          $('.title-bar').hide();
+          $('.navbar').removeClass('navbar-fixed');
+          $(document.body).addClass('sticky-menu');
       } else {
-        removeToggleMenu();
-        removeToggleNavbar();
-        removeStickyMenu();
+        $('.title-bar').hide();
+        $(document.body).removeClass('sticky-menu');
       }
   };
   navbarHandler();
